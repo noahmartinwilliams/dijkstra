d4d2cb10fdd3ca2ce0d82ce470b0d71ab2ea4c2d
skynet1 <thecrazymancan@gmail.com>
2020-04-09 14:08:09 -0500

    Added treeNode launcher.

--- dijkstra.go
+++ dijkstra.go
@@ -1,17 +1,71 @@
 package dijkstra
 
-func launchNodes(retc chan []string, endName string, inputc chan dest) map[string]chan robot {
-	ret := make(map[string]chan robot)
-	for input := range(inputc) {
-		_, ok := ret[input.source]
-		if !ok {
-			ret[input.source] = node(retc, endName, input.source)
-		}
-		_, ok = ret[input.dest]
-		if !ok {
-			ret[input.dest] = node(retc, endName, input.dest)
-		}
+import "sync"
 
+func launchNodes(retc chan []string, endName string, inputc chan dest) chan map[string]chan robot {
+	retc2 := make(chan map[string]chan robot)
+	go func() {
+		ret := make(map[string]chan robot)
+		for input := range(inputc) {
+			_, ok := ret[input.source]
+			if !ok {
+				ret[input.source] = node(retc, endName, input.source)
+			}
+			_, ok = ret[input.dest]
+			if !ok {
+				ret[input.dest] = node(retc, endName, input.dest)
+			}
+
+		}
+		retc2 <- ret
+	} ()
+	return retc2
+}
+
+func splitter(inputc chan dest, out1 chan dest, out2 chan dest) {
+	go func() {
+
+	defer close(out1)
+	defer close(out2)
+	for input := range(inputc) {
+		out1 <- input
+		out2 <- input
 	}
+
+	} ()
+}
+
+func collectNodes(inputc chan dest) map[string][]dest {
+	ret := make(map[string][]dest)
+	for input := range(inputc) {
+		list, ok := ret[input.source]
+		if ok {
+			ret[input.source] = append(list, input)
+		} else {
+			ret[input.source] = []dest{input}
+		}
+	}
+	return ret
+}
+
+func launchTreeNodes(endName string, startName string, inputc chan dest) []string {
+	retc := make(chan []string)
+	inputc2 := make(chan dest)
+	inputc3 := make(chan dest)
+	splitter(inputc, inputc2, inputc3)
+
+	mpc := launchNodes(retc, endName, inputc2)
+	dests := collectNodes(inputc3)
+
+	mp := <-mpc
+	var wg sync.WaitGroup
+	wg.Add(1)
+	inc := treeNode(dests, &wg, startName, mp)
+	inc <- robot{path:[]string{}, pathLength:0.0}
+	wg.Wait()
+	for _, value := range(mp) {
+		close(value)
+	}
+	ret := <-retc
 	return ret
 }
--- dijkstra_test.go
+++ dijkstra_test.go
@@ -12,9 +12,23 @@ func TestLaunchNodes(t *testing.T) {
 		inputc <- dest{source:"b", dest:"c", pathLength:2}
 	} ()
 
-	mp := launchNodes(retc, endName, inputc)
+	mpc := launchNodes(retc, endName, inputc)
+	mp := <-mpc
 	_, ok := mp["a"]
 	if !ok {
 		t.Errorf("Error: launchNodes failed to launch first node.")
+	}
+}
+
+func TestLaunchTreeNodes(t *testing.T) {
+	inputc := make(chan dest)
+	go func() {
+		defer close(inputc)
+		inputc <- dest{source:"a", dest:"b", pathLength:1}
+		inputc <- dest{source:"b", dest:"c", pathLength:2}
+	} ()
+	str := launchTreeNodes("c", "a", inputc)
+	if str[2] != "c" {
+		t.Errorf("Error: launchTreeNodes did not return proper end node.")
 	}
 }
